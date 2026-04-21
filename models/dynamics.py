a80c1d5bd3fe737f6f5891dc9b47f7679fbf6b1b
rohithcontrol-18 <rohithproject18@gmail.com>
2026-04-21 20:35:54 +0530

    Update dynamics.py

--- models/dynamics.py
+++ models/dynamics.py
@@ -1,1 +1,12 @@
+import numpy as np
 
+def F_matrix(dt):
+    return np.array([
+        [1,0,dt,0],
+        [0,1,0,dt],
+        [0,0,1,0],
+        [0,0,0,1]
+    ])
+
+def propagate(x, dt):
+    return F_matrix(dt) @ x
