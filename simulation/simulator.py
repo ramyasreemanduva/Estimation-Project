25ef88d053baa05badb44d93c29ee169d1749e57
ramyasreemanduva <ramyasreemanduva72@gmail.com>
2026-04-21 23:21:07 +0100

    Update simulator.py
    Simulation

--- simulation/simulator.py
+++ simulation/simulator.py
@@ -1,1 +1,10 @@
+def simulate_1D(steps, dt):
+    x, v = 0, 10
+    data = []
+    for _ in range(steps):
+        x += v * dt
+        data.append([x, v])
+    return np.array(data)
 
+def measure_1D(true):
+    return true[:,0] + np.random.randn(len(true))*1.5
