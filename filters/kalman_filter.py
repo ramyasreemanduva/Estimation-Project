a2c1ee85db6dcbc815dbccdd25e15c82934455fc
nikkiappu1999-cyber <nikkiappu1999@gmail.com>
2026-04-22 12:51:41 +0100

    Update kalman_filter.py

--- filters/kalman_filter.py
+++ filters/kalman_filter.py
@@ -1,1 +1,16 @@
+import numpy as np
+
+def kf_step(x, P, z, A, H, Q, R):
+    # Predict
+    x = A @ x
+    P = A @ P @ A.T + Q
+
+    # Update
+    S = H @ P @ H.T + R
+    K = P @ H.T @ np.linalg.inv(S)
+
+    x = x + (K @ (z - H @ x)).flatten()
+    P = (np.eye(len(x)) - K @ H) @ P
+
+    return x, P
 
