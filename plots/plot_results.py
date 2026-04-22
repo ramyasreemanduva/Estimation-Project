0a5be36c2755e945a9356a08e9b2c331ae112546
ramyasreemanduva <ramyasreemanduva72@gmail.com>
2026-04-22 21:16:59 +0100

    Update plot_results.py

--- plots/plot_results.py
+++ plots/plot_results.py
@@ -1,24 +1,17 @@
 import matplotlib.pyplot as plt
-# Position Plot
-def plot_position(x_true, estimates):
-    plt.figure()
-    plt.plot(x_true[:, 0], label="True Position")
-    plt.plot(estimates[:, 0], label="Estimated Position")
-    plt.xlabel("Time step")
-    plt.ylabel("Position")
-    plt.title("Day 1: Position Estimation")
-    plt.legend()
-    plt.grid()
-    plt.show()
 
-# Velocity Plot
-def plot_velocity(x_true, estimates):
+def plot_trajectory(true, est):
     plt.figure()
-    plt.plot(x_true[:, 1], label="True Velocity")
-    plt.plot(estimates[:, 1], label="Estimated Velocity")
-    plt.xlabel("Time step")
-    plt.ylabel("Velocity")
-    plt.title("Day 1: Velocity Estimation")
+
+    plt.plot(true[:, 0], true[:, 1], label="True Path")
+    plt.plot(est[:, 0], est[:, 1], '--', label="Estimated Path")
+
+    plt.xlabel("X position")
+    plt.ylabel("Y position")
+    plt.title("Day 2: Circular Motion Tracking")
+
     plt.legend()
     plt.grid()
+    plt.axis("equal")
+
     plt.show()
