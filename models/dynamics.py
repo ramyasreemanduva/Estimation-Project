e256c9a1b2f00f65e6219f44c89b81696dfee7ff
rohithcontrol-18 <rohithproject18@gmail.com>
2026-04-23 00:21:55 +0530

    Update dynamics.py

--- models/dynamics.py
+++ models/dynamics.py
@@ -1,13 +1,5 @@
-
 import numpy as np
-
-def F_1D(dt):
-    A = np.array([
-        [1, dt],
-        [0, 1]
-    ])
-    return A
-
-def H_1D():
-    H = np.array([[1, 0]])
-    return H
+ def circular_motion(theta, R):
+    x = R * np.cos(theta)
+    y = R * np.sin(theta)
+    return x, y
