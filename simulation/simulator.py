85bcccd743052917862b84c8db92e3513af4e203
ramyasreemanduva <ramyasreemanduva72@gmail.com>
2026-04-23 15:43:05 +0100

    Update simulator.py

--- simulation/simulator.py
+++ simulation/simulator.py
@@ -1,20 +1,23 @@
 import numpy as np
-from models.dynamics import circular_motion, circular_velocity
+from models.dynamics import apply_constraints
 
-def simulate_circular(steps, dt, R, omega):
-    theta = 0.0
+def simulate_2D(steps, dt):
+    x, y = 0, 0
+    vx, vy = 10, 0.5
+
     data = []
 
     for _ in range(steps):
-        x, y = circular_motion(theta, R)
-        vx, vy = circular_velocity(theta, omega, R)
+        x += vx * dt
+        y += vy * dt
+
+        # Apply constraint
+        y = apply_constraints(y)
 
         data.append([x, y, vx, vy])
-        theta += omega * dt
 
     return np.array(data)
 
 
 def measure_2D(true_states):
-    # Add Gaussian noise
     return true_states[:, :2] + np.random.randn(len(true_states), 2) * 1.5
