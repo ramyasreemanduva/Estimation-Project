d9b21e3e4e9da4f2a98eaa611d9fbb77d60a72b3
ramyasreemanduva <ramyasreemanduva72@gmail.com>
2026-04-23 15:41:24 +0100

    Update dynamics.py

--- models/dynamics.py
+++ models/dynamics.py
@@ -1,24 +1,5 @@
 import numpy as np
 
-def circular_motion(theta, R):
-    x = R * np.cos(theta)
-    y = R * np.sin(theta)
-    return x, y
-
-def circular_velocity(theta, omega, R):
-    vx = -R * omega * np.sin(theta)
-    vy = R * omega * np.cos(theta)
-    return vx, vy
-
-def F_2D(dt):
-    return np.array([
-        [1, 0, dt, 0],
-        [0, 1, 0, dt],
-        [0, 0, 1, 0],
-        [0, 0, 0, 1]
-    ])
-    import numpy as np
-
 # Circular Motion 
 
 def circular_motion(theta, R):
