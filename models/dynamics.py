84a8e25443feb53370ebedf303906066c8d90ef8
rohithcontrol-18 <rohithproject18@gmail.com>
2026-04-24 16:40:20 +0530

    Update dynamics.py

--- models/dynamics.py
+++ models/dynamics.py
@@ -7,7 +7,6 @@ def circular_motion(theta, R):
     y = R * np.sin(theta)
     return x, y
 
-
 # Circular Velocity 
 
 def circular_velocity(theta, omega, R):
@@ -27,5 +26,35 @@ def F_2D(dt):
 
 
 # Lane Constraint
+
 def apply_constraints(y):
     return np.clip(y, -2, 2)
+
+# NEW: Beacons 
+
+def get_beacons():
+    return np.array([
+        [-20, 10],
+        [50, -10],
+        [120, 15]
+    ])
+
+
+# Nonlinear Measurement Function
+
+def h(x, beacon):
+    dx = x[0] - beacon[0]
+    dy = x[1] - beacon[1]
+    return np.array([np.sqrt(dx**2 + dy**2)])
+
+# Jacobian of Measurement
+
+def H_jacobian(x, beacon):
+    dx = x[0] - beacon[0]
+    dy = x[1] - beacon[1]
+
+    r = np.sqrt(dx**2 + dy**2)
+
+    return np.array([
+        [dx/r, dy/r, 0, 0]
+    ])
