d03b3464ae0679dab5d1e966856191de4ef51901
nikkiappu1999-cyber <nikkiappu1999@gmail.com>
2026-04-24 17:30:41 +0100

    Update kalman_filter.py

--- filters/kalman_filter.py
+++ filters/kalman_filter.py
@@ -1,45 +1,24 @@
 import numpy as np
-from models.dynamics import H_jacobian
-
-# EKF Prediction
+from models.dynamics import F_2D, H_jacobian
 
 def ekf_predict(x, P, Q, dt):
-    A = np.array([
-        [1, 0, dt, 0],
-        [0, 1, 0, dt],
-        [0, 0, 1, 0],
-        [0, 0, 0, 1]
-    ])
-
+    A = F_2D(dt)
     x = A @ x
     P = A @ P @ A.T + Q
-
     return x, P
 
-
-# EKF Multi-Beacon Update
-
-def ekf_update_multi(x, P, z_all, beacons, R):
-
-    H_list = []
-    y_list = []
-
-    for i, beacon in enumerate(beacons):
-        dx = x[0] - beacon[0]
-        dy = x[1] - beacon[1]
-
-        r = np.sqrt(dx**2 + dy**2) + 1e-6
-
-        H_list.append([dx/r, dy/r, 0, 0])
-        y_list.append(z_all[i] - r)
+def ekf_update_multi(x, P, z_all, beacons, R_mat):
+    H_list, y_list = [], []
+    for i, b in enumerate(beacons):
+        H_i = H_jacobian(x, b)
+        dist_pred = np.sqrt((x[0]-b[0])**2 + (x[1]-b[1])**2)
+        H_list.append(H_i[0])
+        y_list.append(z_all[i] - dist_pred)
 
     H = np.array(H_list)
     y = np.array(y_list).reshape(-1, 1)
-
-    S = H @ P @ H.T + R
+    S = H @ P @ H.T + R_mat
     K = P @ H.T @ np.linalg.inv(S)
-
     x = x + (K @ y).flatten()
     P = (np.eye(4) - K @ H) @ P
-
     return x, P
