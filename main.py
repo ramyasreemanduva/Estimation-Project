d450439c73f4f03f3cbf9a684e1834c268280fab
ramyasreemanduva <ramyasreemanduva72@gmail.com>
2026-04-24 17:20:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,25 @@
 import numpy as np
 from simulation.simulator import get_track_geometry, measure_beacons
+from filters.kalman_filter import ekf_predict, ekf_update_multi
+from models.dynamics import get_beacons
 from plots.plot_results import plot_trajectory
-# from filters.kalman_filter import ekf_predict, ekf_update_multi
 
-dt = 0.01 # 100 Hz [cite: 20]
-steps = 6000 # Enough steps for one lap at 10m/s
-
+dt, steps = 0.01, 5000 # 100 Hz frequency 
 paths = get_track_geometry(steps, dt)
-true_x, true_y = paths[0]
-inner_bound, outer_bound = paths[1], paths[2]
+true_states = np.column_stack([paths[0][0], paths[0][1], np.gradient(paths[0][0], dt), np.gradient(paths[0][1], dt)])
 
-vx = np.gradient(true_x, dt)
-vy = np.gradient(true_y, dt)
-true_states = np.column_stack([true_x, true_y, vx, vy])
-
-# Beacons b1, b2, b3 outside lane region [cite: 18, 37]
-beacons = np.array([[20, 60], [20, -60], [180, 0]])
+beacons = get_beacons()
 measurements = measure_beacons(true_states, beacons)
 
-x_est = np.array([true_x[0], true_y[0], vx[0], vy[0]])
+x_est = true_states[0].copy()
 P = np.eye(4) * 0.1
-# Reduced Q to smooth out lateral and velocity estimates
-Q = np.diag([0.001, 0.001, 0.01, 0.01]) 
-R = np.eye(len(beacons)) * (1.5**2) # [cite: 19]
+Q = np.diag([0.001, 0.001, 0.01, 0.01]) # Process noise assumption 
+R = np.eye(len(beacons)) * (1.5**2)     # Measurement noise variance 
 
 estimates = []
-for k in range(len(true_states)):
-    # x_est, P = ekf_predict(x_est, P, Q, dt)
-    # x_est, P = ekf_update_multi(x_est, P, measurements[k], beacons, R)
+for k in range(steps):
+    x_est, P = ekf_predict(x_est, P, Q, dt)
+    x_est, P = ekf_update_multi(x_est, P, measurements[k], beacons, R)
     estimates.append(x_est.copy())
 
-estimates = np.array(estimates)
-plot_trajectory(true_states, estimates, beacons, inner_bound, outer_bound)
+plot_trajectory(true_states, np.array(estimates), beacons, paths[1], paths[2])
