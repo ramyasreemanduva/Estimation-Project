1dbceb31fd4d94c9d4e2924f71b023ef58238fce
ramyasreemanduva <ramyasreemanduva72@gmail.com>
2026-04-24 15:31:41 +0100

    Update simulator.py

--- simulation/simulator.py
+++ simulation/simulator.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-# SIMULATION
+# TRUE TRACK
 
 def simulate_2D(steps, dt):
 
@@ -9,36 +9,38 @@ def simulate_2D(steps, dt):
     d = 100.0
     v = 10.0
 
+    # Circle centers
     A = np.array([0.0, 0.0])
     B = np.array([d, 0.0])
 
-    # ---------- CORRECT TANGENT ANGLE ----------
+    # ---------- Correct tangent angle ----------
     alpha = np.arcsin((R - rho) / d)
 
-    # ---------- TANGENT POINTS ----------
+    # ---------- Tangent points ----------
     TL = A + R * np.array([ np.sin(alpha),  np.cos(alpha)])
     TR = B + rho * np.array([ np.sin(alpha),  np.cos(alpha)])
 
     BL = A + R * np.array([-np.sin(alpha), -np.cos(alpha)])
     BR = B + rho * np.array([-np.sin(alpha), -np.cos(alpha)])
 
-    # ---------- SEGMENT LENGTHS ----------
-    L1 = np.pi * R
-    L2 = np.linalg.norm(TR - TL)
-    L3 = np.pi * rho
-    L4 = np.linalg.norm(BL - BR)
+    # ---------- Segment lengths ----------
+    L_left = np.pi * R
+    L_top = np.linalg.norm(TR - TL)
+    L_right = np.pi * rho
+    L_bottom = np.linalg.norm(BL - BR)
 
-    total = L1 + L2 + L3 + L4
-    s_vals = np.linspace(0, total, steps)
+    total_length = L_left + L_top + L_right + L_bottom
+
+    s_vals = np.linspace(0, total_length, steps)
 
     data = []
 
     for s in s_vals:
 
-        # -------- LEFT ARC --------
-        if s < L1:
-            theta0 = np.arctan2(TL[1], TL[0])
-            theta = theta0 - s / R
+        # ---------- LEFT ARC ----------
+        if s < L_left:
+            theta_start = np.arctan2(TL[1], TL[0])
+            theta = theta_start - s / R
 
             x = R * np.cos(theta)
             y = R * np.sin(theta)
@@ -46,20 +48,20 @@ def simulate_2D(steps, dt):
             vx = -v * np.sin(theta)
             vy =  v * np.cos(theta)
 
-        # -------- TOP SLOPED STRAIGHT --------
-        elif s < L1 + L2:
-            t = (s - L1) / L2
+        # ---------- TOP STRAIGHT ----------
+        elif s < L_left + L_top:
+            t = (s - L_left) / L_top
             pos = TL + t * (TR - TL)
 
             x, y = pos
-            direction = (TR - TL) / L2
+            direction = (TR - TL) / L_top
 
             vx, vy = v * direction
 
-        # -------- RIGHT ARC --------
-        elif s < L1 + L2 + L3:
-            theta0 = np.arctan2(TR[1], TR[0] - d)
-            theta = theta0 - (s - (L1 + L2)) / rho
+        # ---------- RIGHT ARC ----------
+        elif s < L_left + L_top + L_right:
+            theta_start = np.arctan2(TR[1], TR[0] - d)
+            theta = theta_start - (s - (L_left + L_top)) / rho
 
             x = d + rho * np.cos(theta)
             y = rho * np.sin(theta)
@@ -67,13 +69,13 @@ def simulate_2D(steps, dt):
             vx = -v * np.sin(theta)
             vy =  v * np.cos(theta)
 
-        # -------- BOTTOM SLOPED STRAIGHT --------
+        # ---------- BOTTOM STRAIGHT ----------
         else:
-            t = (s - (L1 + L2 + L3)) / L4
+            t = (s - (L_left + L_top + L_right)) / L_bottom
             pos = BR + t * (BL - BR)
 
             x, y = pos
-            direction = (BL - BR) / L4
+            direction = (BL - BR) / L_bottom
 
             vx, vy = v * direction
 
@@ -81,7 +83,8 @@ def simulate_2D(steps, dt):
 
     return np.array(data)
 
-# MEASUREMENTS 
+
+# MEASUREMENTS
 
 def measure_beacons(states, beacons):
 
